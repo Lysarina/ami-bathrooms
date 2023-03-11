8ec8241931e354979880f357eaba592b3a5732c9
Hallvaeb <hallvard.bjorgen@gmail.com>
2023-03-11 19:11:49 +0000

    tiny changes

--- server/subscriber.py
+++ server/subscriber.py
@@ -2,20 +2,22 @@ import paho.mqtt.client as mqtt
 
 CLIENT_NAME = "subscriber"
 #BROKER_URL = "localhost"
-BROKER_URL = "165.232.73.30"
+BROKER_URL = "165.22.31.23"
 USERNAME = "hallvard"
-PASSWORD = "MQTTPass3"
+PASSWORD = "HallvardPass3"
 BROKER_PORT = 1883
+TOPICS = ["sensors/motion", "sensors/temperature", "sensors/humidity", "sensors/light", "queue/bathroom1"]
+
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("sensors/motion")
-    client.subscribe("ScentStick/a/b")
+    list(map(client.subscribe, TOPICS))
+    print("subbed_topics", TOPICS);
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    if msg.topic == "sensors/motion":
-        print("motion")
+    print(f"New message on: {msg.topic}, msg.payload: {msg.payload}")
 
 def init():
     client = mqtt.Client(CLIENT_NAME)
