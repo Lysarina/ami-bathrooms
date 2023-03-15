8ce7bfbf69d1397e978d45ace406e6936800574a
Ella Viirola <ella.viirola@outlook.com>
2023-03-15 10:40:28 +0000

    Fixed queue system frontend side

--- website/app.js
+++ website/app.js
@@ -2,10 +2,11 @@ var noOfBathrooms;
 var userID;
 
 function onConnect(){
-    topic =  "sensors/motion";
-    console.log("Subscribing to topics")
-    client.subscribe(topic);
-    client.subscribe("notify")
+    var topics = ["sensors/motion", "sensors/light", "sensors/temperature", "sensors/humidity", "queue"]
+    for (let i = 0; i < topics.length; i++){
+        client.subscribe(topics[i])
+        console.log("Subscribed to topic " + topics[i])
+    }
 }
 
 function onConnectionLost(responseObject){
@@ -13,19 +14,31 @@ function onConnectionLost(responseObject){
     if(responseObject !=0){
         console.log("ERROR:"+ responseObject.errorMessage);
     }
+    document.getElementById("connectionWarning").innerHTML = "Connection to server lost. Please refresh the page.";
 }
 
 function onMessageArrived(message){
-    console.log("OnMessageArrived: "+message.payloadString);
-    const data = JSON.parse(message.payloadString);
     const topic = message.destinationName;
+    console.log("Received a message from " + topic + ": " + message.payloadString);
+    const data = JSON.parse(message.payloadString);
     
-    if (topic === "notify") {
-        // expects: user id, notify off
-        // maybe: bathroom that is free?
-        if (message.payloadString === "off") {
-            alert("Free bathroom!")
+    if (topic === "queue") {
+        // expects: user id, 0, bathroomID
+        if (data.userID === userID && data.waiting === 0){
+            alert("Bathroom " + data.bathroomID + " is now free!");
+            document.getElementById("notifyMsg").innerHTML = "";
+            document.getElementById("queueWarning").innerHTML = "";
+        } else if (data.userID !== userID && data.waiting === 0) {
+            document.getElementById("queueWarning").innerHTML = "There are still people before you in line.";
         }
+    } else if (topic === "sensors/light") {
+        var lights;
+        if (data.data>0){
+            lights = "On"
+        } else {
+            lights = "Off"
+        }
+        document.getElementById(data.sensor + data.bathroomID).innerHTML = lights;
     } else {
         document.getElementById(data.sensor + data.bathroomID).innerHTML = data.data;
     }
@@ -53,30 +66,27 @@ function startDisconnect(){
     console.log("Disconnected from broker");
 }
 
-function publishNotifyMessage(){
+function publishQueueMessage(){
     // publish json with  user ID and on message
-    msgJSON = "{"+
-        + "userID: " + userID +
-        + "notify: 1" +
+    msgJSON = "{"
+        + "\"userID\": \"" + userID + "\", "
+        + "\"waiting\": 1" +
     "}";
-    msg = "on";
-    topic = "notify";
+    topic = "queue";
     Message = new Paho.MQTT.Message(msgJSON);
     Message.destinationName = topic;
     client.send(Message);
     console.log("Message to topic "+topic+" is sent")
-    document.getElementById("notifymsg").innerHTML = "You will be notified when a bathroom is available. Do not refresh the page!";
+    document.getElementById("notifyMsg").innerHTML = "You will be notified when a bathroom is available. Do not refresh the page!";
 }
 
 function startConnect(){
-    clientID = "Webapp";
     host = "165.22.31.23";   
     port = "8080";
     userId  = "ella";  
     passwordId = "EllaPass3";  
-    console.log("Connecting to " + host + "on port " +port);
-    console.log("Using the client ID " + clientID);
-    client = new Paho.MQTT.Client(host,Number(port),clientID);
+    console.log("Connecting to " + host + " on port " +port);
+    client = new Paho.MQTT.Client(host, Number(port), userID);
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
     client.connect({
@@ -97,14 +107,19 @@ function startConnect(){
 // }
 
 window.onload = function () {
-    // if (!('hasCodeRunBefore' in localStorage)){
-        startConnect();
-        // readTextFile("/bathrooms.txt");
-        noOfBathrooms = 2;
-        for (let i = 1; i <= noOfBathrooms; i++){
-        document.getElementById("bathrooms").innerHTML += "<tr><td>Bathroom " + i + "</td><td id=\"motion" + i + "\">Occupied?</td><td id=\"temperature" + i + "\">Temperature</td><td id=\"humidity" + i + "\">Humidity</td></tr>"
-        }
-        // generate random user id
-    //     localStorage.setItem("hasCodeRunBefore", true);
-    // }
+    // generate random user ID
+    userID = "user" + Math.floor(Math.random() * 500);
+    console.log("UserID " + userID);
+
+    startConnect();
+    // readTextFile("/bathrooms.txt");
+    noOfBathrooms = 2;
+    for (let i = 1; i <= noOfBathrooms; i++){
+        document.getElementById("bathrooms").innerHTML += "<tr><td>Bathroom " + i + "</td>" + 
+            "<td id=\"motion" + i + "\">Occupied?</td>" + 
+            "<td id=\"temperature" + i + "\">Temperature</td>" + 
+            "<td id=\"humidity" + i + "\">Humidity</td>" + 
+            "<td id=\"light" + i + "\">Lights</td>" +
+            "</tr>"
+    }
 }
--- website/pubmsg.sh
+++ website/pubmsg.sh
@@ -1,1 +1,1 @@
-mosquitto_pub -d -t sensors/motion -u ella -P EllaPass3 -m "Hello World" -h 165.22.31.23
+mosquitto_pub -d -t queue -u ella -P EllaPass3 -m "{\"userID\": \"user45\", \"waiting\":0, \"bathroomID\": 1}" -h 165.22.31.23
