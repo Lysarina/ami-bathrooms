57289f0eacf4e599eabdbac38064fa7f205d0da5
Hallvaeb <hallvard.bjorgen@gmail.com>
2023-03-16 10:53:24 +0000

    Add arduino code to project folder

---
+++ BusyBathroomsArduino/arduino_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID_WIFI "saojose"
+#define SECRET_PASS_WIFI "abril2018"
+// #define SECRET_SSID_WIFI "Britney Bitch"
+// #define SECRET_PASS_WIFI "WillIAmAnd"
+
+#define MQTT_USERNAME "hallvard"
+#define MQTT_PASSWORD "HallvardPass3"
